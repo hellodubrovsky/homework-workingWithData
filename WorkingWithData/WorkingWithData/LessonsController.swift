b278b634cfc4067ebe7e42514c37ebdb16c9fd2b
Илья Дубровский <hellodubrovsky@icloud.com>
2023-01-10 22:46:49 +0300

    Создал проект, в котором реализовал tabBar для разделения на лекции и практические задания.

---
+++ WorkingWithData/WorkingWithData/LessonsController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WorkingWithData
+//
+//  Created by Илья Дубровский on 10.01.2023.
+//
+
+import UIKit
+
+class LessonsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
---
+++ WorkingWithData/WorkingWithData/PracticeController.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeController.swift
+//  WorkingWithData
+//
+//  Created by Илья Дубровский on 10.01.2023.
+//
+
+import UIKit
+
+class PracticeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
